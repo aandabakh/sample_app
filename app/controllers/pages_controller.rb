a90cdc1bf356a6f16720c512f30c12f2aeb73328
Baye Abdoul <aandabakh@gmail.com>
2012-10-20 23:07:01 +0000

    Added user microposts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
   def home
-	@titre = "Accueil"
+    @titre = "Accueil"
+    @micropost = Micropost.new if signed_in?
+    @feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 3) if signed_in?
   end
 
   def contact
-	@titre = "Contact"
+    @titre = "Contact"
   end
 
   def about
-	@titre = "A Propos"
+    @titre = "A Propos"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page], :per_page => 3)
     @titre = @user.nom
   end
 
@@ -53,10 +54,6 @@ class UsersController < ApplicationController
   end
 
   private
-    def authenticate
-      deny_access unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
